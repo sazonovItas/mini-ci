512173c54e0caa05d8abcbd3f1bced0568a3f1e2
sazonovItas <itassudo@gmail.com>
2025-12-17 16:44:34 +0300

    wip: added dockerfiles for worker and workflower

--- Makefile
+++ Makefile
@@ -13,6 +13,28 @@ fmt: ## Run go fmt against code
 vet: ## RUN go vet against code
 	go vet ./...
 
+##@ Dependencies
+
+## Location to install dependencies to
+LOCALBIN ?= $(shell pwd)/bin
+$(LOCALBIN):
+		mkdir -p $(LOCALBIN)
+
+## Tool Binaries
+SQLC ?= $(LOCALBIN)/sqlc
+
+## Tool Versions
+SQLC_VERSION ?= v1.30.0
+
+.PHONY: sqlc-gen
+sqlc-gen: sqlc
+	$(SQLC) generate
+
+.PHONY: sqlc
+sqlc: $(SQLC)
+$(SQLC): $(LOCALBIN)
+	$(call go-install-tool,$(SQLC),github.com/sqlc-dev/sqlc/cmd/sqlc,$(SQLC_VERSION))
+
 # go-install-tool will 'go install' any package with custom target and name of binary, if it doesn't exist
 # $1 - target path with name of binary
 # $2 - package url which can be installed
--- workflower/engine/engine.go
+++ workflower/engine/engine.go
@@ -1,23 +1,57 @@
 package engine
 
 import (
+	"context"
+
+	"github.com/sazonovItas/mini-ci/core/events"
+	"github.com/sazonovItas/mini-ci/core/events/watcher"
 	"github.com/sazonovItas/mini-ci/workflower/db"
 )
 
 type Engine struct {
-	builds *db.BuildFactory
-	jobs   *db.JobFactory
-	tasks  *db.TaskFactory
+	bus events.Bus
 
-	planner Planner
+	workflows *db.WorkflowFactory
+	builds    *db.BuildFactory
+	jobs      *db.JobFactory
+	tasks     *db.TaskFactory
+
+	planner  Planner
+	watchers []*watcher.Watcher
 }
 
-func New(builds *db.BuildFactory, jobs *db.JobFactory, tasks *db.TaskFactory) *Engine {
+func New(
+	bus events.Bus,
+	workflows *db.WorkflowFactory,
+	builds *db.BuildFactory,
+	jobs *db.JobFactory,
+	tasks *db.TaskFactory,
+) *Engine {
 	return &Engine{
-		builds: builds,
-		jobs:   jobs,
-		tasks:  tasks,
+		bus: bus,
+
+		workflows: workflows,
+		builds:    builds,
+		jobs:      jobs,
+		tasks:     tasks,
 
 		planner: NewPlanner(),
+		watchers: []*watcher.Watcher{
+			watcher.NewWatcher(bus, NewBuildProcessor(builds, jobs, bus)),
+			watcher.NewWatcher(bus, NewJobProcessor(jobs, tasks, bus)),
+			watcher.NewWatcher(bus, NewTaskProcessor(tasks, bus)),
+		},
+	}
+}
+
+func (e *Engine) Start(ctx context.Context) {
+	for _, w := range e.watchers {
+		w.Start(ctx)
+	}
+}
+
+func (e *Engine) Stop() {
+	for _, w := range e.watchers {
+		w.Stop()
 	}
 }
